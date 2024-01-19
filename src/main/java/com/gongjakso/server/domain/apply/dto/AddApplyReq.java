c8cd115ce19f8cf4d74a02a4b3b825679dcee5b7
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-01-19 18:39:30 +0900

    #5 Apply 생성 기능 성공
    issue : member_id header로 가져오기

--- src/main/java/com/gongjakso/server/domain/apply/dto/AddApplyReq.java
+++ src/main/java/com/gongjakso/server/domain/apply/dto/AddApplyReq.java
@@ -2,17 +2,24 @@ package com.gongjakso.server.domain.apply.dto;
 
 import com.gongjakso.server.domain.apply.entity.Apply;
 import com.gongjakso.server.domain.apply.enumerate.PostType;
+import com.gongjakso.server.domain.member.entity.Member;
 
 public record AddApplyReq(
+        Member memberId,
         String application,
         String recruit_part,
-        PostType type
+        PostType type,
+        Boolean is_pass,
+        Boolean is_open
 ) {
     public Apply toEntity(){
         return Apply.builder()
+                .member(memberId)
                 .application(application)
                 .recruit_part(recruit_part)
                 .type(type)
+                .is_pass(false)
+                .is_open(false)
                 .build();
     }
 }
