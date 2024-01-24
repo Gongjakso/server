bf29d8492e11b11120786374cfdfb2eb2a6ca4b4
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2024-01-24 23:05:27 +0900

    [#3] chore: 공통 응답 객체, 에러 응답 객체 구현

--- src/main/java/com/gongjakso/server/global/exception/ErrorResponse.java
+++ src/main/java/com/gongjakso/server/global/exception/ErrorResponse.java
@@ -1,4 +1,14 @@
 package com.gongjakso.server.global.exception;
 
-public class ErrorResponse {
+import java.time.LocalDateTime;
+
+
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        Integer code,
+        String message) {
+
+    public ErrorResponse(ErrorCode errorcode) {
+        this(LocalDateTime.now().withNano(0), errorcode.getCode(), errorcode.getMessage());
+    }
 }
