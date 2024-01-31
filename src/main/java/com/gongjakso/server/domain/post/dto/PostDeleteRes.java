dd0149b80151557d842c66052d8e3f83c6b70896
yumzen <yeom.ji.eun35@gmail.com>
2024-01-31 21:12:28 +0900

    feat: post delete response DTO 수정

--- src/main/java/com/gongjakso/server/domain/post/dto/PostDeleteRes.java
+++ src/main/java/com/gongjakso/server/domain/post/dto/PostDeleteRes.java
@@ -3,18 +3,14 @@ package com.gongjakso.server.domain.post.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 public class PostDeleteRes {
     private Long postId;
     private Long memberId;
-    private LocalDateTime deletedAt;
 
     @Builder
-    public PostDeleteRes(Long postId, Long memberId, LocalDateTime deletedAt){
+    public PostDeleteRes(Long postId, Long memberId){
         this.postId = postId;
         this.memberId = memberId;
-        this.deletedAt = deletedAt;
     }
 }
