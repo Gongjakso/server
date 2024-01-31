442e3959226a382a80feae6813d030251f59fe1c
yumzen <yeom.ji.eun35@gmail.com>
2024-01-31 21:14:21 +0900

    feat: post service 수정

--- src/main/java/com/gongjakso/server/domain/post/service/PostService.java
+++ src/main/java/com/gongjakso/server/domain/post/service/PostService.java
@@ -1,13 +1,18 @@
 package com.gongjakso.server.domain.post.service;
 
+import com.gongjakso.server.domain.member.entity.Member;
 import com.gongjakso.server.domain.post.dto.PostDeleteRes;
 import com.gongjakso.server.domain.post.dto.PostReq;
 import com.gongjakso.server.domain.post.dto.PostRes;
 import com.gongjakso.server.domain.post.entity.Post;
 import com.gongjakso.server.domain.post.repository.PostRepository;
+import com.gongjakso.server.global.exception.ApplicationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.gongjakso.server.global.exception.ErrorCode.NOT_FOUND_EXCEPTION;
+import static com.gongjakso.server.global.exception.ErrorCode.UNAUTHORIZED_EXCEPTION;
 
 
 @Service
@@ -16,10 +21,44 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostService {
     private final PostRepository postRepository;
 
+
     @Transactional
-    public PostRes modify(Long id, PostReq req) {
-        Post entity = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException());
+    public PostRes create(Member member, PostReq req) {
+        Post entity = new Post(req.getTitle(), member, req.getContents(), req.getStatus(), req.getStartDate(), req.getEndDate(),
+                req.getFinishDate(), req.getMaxPerson(), req.getMeetingMethod(), req.getMeetingArea(),  req.isQuestionMethod(),
+                req.getQuestionLink(), req.isPostType());
+
+        postRepository.save(entity);
+
+        return PostRes.builder()
+                .postId(entity.getPostId())
+                .memberId(entity.getMember().getMemberId())
+                .title(entity.getTitle())
+                .contents(entity.getContents())
+                .status(entity.getStatus())
+                .startDate(entity.getStartDate())
+                .endDate(entity.getEndDate())
+                .finishDate(entity.getFinishDate())
+                .maxPerson(entity.getMaxPerson())
+                .meetingMethod(entity.getMeetingMethod())
+                .meetingArea(entity.getMeetingArea())
+                .questionMethod(entity.isQuestionMethod())
+                .questionLink(entity.getQuestionLink())
+                .postType(entity.isPostType())
+                .createdAt(entity.getCreatedAt())
+                .modifiedAt(entity.getModifiedAt())
+                .deletedAt(entity.getDeletedAt())
+                .build();
+
+    }
+    @Transactional
+    public PostRes modify(Member member, Long id, PostReq req) {
+        Post entity = postRepository.findByPostIdAndDeletedAtIsNull(id)
+                .orElseThrow(() -> new ApplicationException(NOT_FOUND_EXCEPTION));
+        if(!member.getMemberId().equals(entity.getMember().getMemberId())){
+            throw new ApplicationException(UNAUTHORIZED_EXCEPTION);
+        }
+
         entity.modify(req);
 
         return PostRes.builder()
@@ -30,12 +69,13 @@ public class PostService {
                 .status(entity.getStatus())
                 .startDate(entity.getStartDate())
                 .endDate(entity.getEndDate())
+                .finishDate(entity.getFinishDate())
                 .maxPerson(entity.getMaxPerson())
                 .meetingMethod(entity.getMeetingMethod())
                 .meetingArea(entity.getMeetingArea())
                 .questionMethod(entity.isQuestionMethod())
                 .questionLink(entity.getQuestionLink())
-                .isProject(entity.isProject())
+                .postType(entity.isPostType())
                 .createdAt(entity.getCreatedAt())
                 .modifiedAt(entity.getModifiedAt())
                 .deletedAt(entity.getDeletedAt())
@@ -45,15 +85,28 @@ public class PostService {
 
 
     @Transactional
-    public PostDeleteRes delete(Long id) {
-        Post entity = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException());
-        entity.delete();
+    public PostDeleteRes delete(Member member, Long id) {
+        Post entity = postRepository.findByPostIdAndDeletedAtIsNull(id)
+                .orElseThrow(() -> new ApplicationException(NOT_FOUND_EXCEPTION));
 
+        postRepository.delete(entity);
         return PostDeleteRes.builder()
                 .postId(entity.getPostId())
                 .memberId(entity.getMember().getMemberId())
-                .deletedAt(entity.getDeletedAt())
                 .build();
     }
+
+    /*
+    public Page<GetProjectRes> getProjects(Pageable page) throws ApplicationException {
+        try {
+            return postRepository.findAll(page).map(projects -> new GetProjectRes(
+                    projects.getPostId(),
+                    projects.getTitle(),
+                    projects.getMeetingArea());
+        } catch (Exception e) {
+            throw new ApplicationException(INVALID_VALUE_EXCEPTION);
+        }
+    }
+
+*/
 }
