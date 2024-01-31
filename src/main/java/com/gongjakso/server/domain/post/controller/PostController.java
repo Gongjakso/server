95a8144eb47400402e2b97610123fac730be174c
yumzen <yeom.ji.eun35@gmail.com>
2024-01-31 21:11:42 +0900

    feat: post controller 수정

--- src/main/java/com/gongjakso/server/domain/post/controller/PostController.java
+++ src/main/java/com/gongjakso/server/domain/post/controller/PostController.java
@@ -4,9 +4,11 @@ import com.gongjakso.server.domain.post.dto.PostDeleteRes;
 import com.gongjakso.server.domain.post.dto.PostReq;
 import com.gongjakso.server.domain.post.dto.PostRes;
 import com.gongjakso.server.domain.post.service.PostService;
+import com.gongjakso.server.global.common.ApplicationResponse;
+import com.gongjakso.server.global.security.PrincipalDetails;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,40 +17,17 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Post", description = "공고 관련 API")
 public class PostController {
     private final PostService postService;
-
-    // @PostMapping
-    // public ResponseEntity<> createPost() {
-    //
-    // }
-
-    /**
-     * 모집글 게시(CREATE)
-     */
-    // @PostMapping("/posts")
-    // public ResponseEntity<PostCreationResponse> createPost(
-    // @RequestBody @Validated PostCreationRequest request,
-    // @AuthenticationPrincipal OAuth2User user) {
-    //
-    // String email = user.getName();
-    // Post post = postService.createPost(request, email);
-    // keywordService.addKeywords(post, request);
-    //
-    // return ResponseEntity.ok(
-    // PostCreationResponse.builder()
-    // .id(post.getId())
-    // .title(post.getThumbnail().getTitle())
-    // .createdDate(post.getCreatedDate())
-    // .build());
-    // }
-
-    @PutMapping("/?{id}")
-    public ResponseEntity<PostRes> modify(@PathVariable("id") Long id, @RequestBody PostReq req) {
-        return ResponseEntity.ok(postService.modify(id, req));
+    @PostMapping("")
+    public ApplicationResponse<PostRes> create(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody PostReq req) {
+        return ApplicationResponse.ok(postService.create(principalDetails.getMember(), req));
+    }
+    @PutMapping("/{id}")
+    public ApplicationResponse<PostRes> modify(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable("id") Long id, @RequestBody PostReq req) {
+        return ApplicationResponse.ok(postService.modify(principalDetails.getMember(), id, req));
     }
 
-    @PatchMapping("/?{id}")
-    public ResponseEntity<PostDeleteRes> delete(@PathVariable("id") Long id){
-        return ResponseEntity.ok(postService.delete(id));
+    @PatchMapping("/{id}")
+    public ApplicationResponse<PostDeleteRes> delete(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable("id") Long id){
+        return ApplicationResponse.ok(postService.delete(principalDetails.getMember(), id));
     }
-    
 }
