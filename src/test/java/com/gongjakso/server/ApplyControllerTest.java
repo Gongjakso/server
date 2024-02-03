3dadffebe5e11bba7679b8d84475be1ed222a512
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-02-03 23:30:34 +0900

    test : start Apply test code

---
+++ src/test/java/com/gongjakso/server/ApplyControllerTest.java
@@ -0,0 +1,42 @@
+package com.gongjakso.server;
+
+import com.gongjakso.server.domain.apply.entity.Apply;
+import com.gongjakso.server.domain.apply.repository.ApplyRepository;
+import com.gongjakso.server.domain.member.entity.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplyControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext context;
+    @Autowired
+    private ApplyRepository applyRepository;
+    @BeforeEach
+    public void setMockMvc(){
+        this.mockMvc= MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    @AfterEach
+    public void cleanUp(){
+//        ApplyRepository.deleteAll();
+    }
+
+    @DisplayName("createApply : 지원하기에 성공한다.")
+    @Test
+    public void createApply() throws Exception{
+        final String url = "/api/v1/apply/1";
+//        Apply savedApply = applyRepository.save(new Apply(1L, ))
+    }
+}
