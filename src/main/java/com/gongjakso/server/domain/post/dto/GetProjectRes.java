cb8f4f606e79a42e75881d6849520d0c2c52d77c
yumzen <yeom.ji.eun35@gmail.com>
2024-02-05 23:25:54 +0900

    feat: GetProjectRes 생성

--- src/main/java/com/gongjakso/server/domain/post/dto/GetProjectRes.java
+++ src/main/java/com/gongjakso/server/domain/post/dto/GetProjectRes.java
@@ -1,2 +1,28 @@
-package com.gongjakso.server.domain.post.dto;public class GetProjectRes {
+package com.gongjakso.server.domain.post.dto;
+
+import com.gongjakso.server.domain.post.enumerate.PostStatus;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+public class GetProjectRes {
+    private Long postId;
+    private String title;
+    private String name; //팀장명
+    private PostStatus status;
+    private LocalDateTime startDate;
+
+    private LocalDateTime finishDate;
+
+    @Builder
+    public GetProjectRes(Long postId, String title, String name, PostStatus status, LocalDateTime startDate, LocalDateTime finishDate){
+        this.postId = postId;
+        this.title = title;
+        this.name = name;
+        this.status = status;
+        this.startDate = startDate;
+        this.finishDate = finishDate;
+    }
+    // 파트명
+    // 스크랩 횟수
 }
