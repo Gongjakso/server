7552c0c8a0c4fd2b8939603e62e3184009cc7a4f
yumzen <yeom.ji.eun35@gmail.com>
2024-02-06 16:29:08 +0900

    feat: PostRepository 변경

--- src/main/java/com/gongjakso/server/domain/post/repository/PostRepository.java
+++ src/main/java/com/gongjakso/server/domain/post/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package com.gongjakso.server.domain.post.repository;
 
+import com.gongjakso.server.domain.post.dto.GetProjectRes;
 import com.gongjakso.server.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,6 +13,11 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     //Page<Post> findAllOrderByCreatedAtDesc(Pageable pageable); //전체 조회(페이징)
 
     //Page<Post> findByCategory(StackName stackname, Pageable pageable);
+    Post findByPostId(Long postId); //applyService
+    @Query("SELECT new com.gongjakso.server.domain.post.dto.GetProjectRes(p.postId, p.title, m.name, p.status, p.startDate, p.finishDate) " +
+            "FROM Post p JOIN p.member m")
+    Page<GetProjectRes> findAllProjects(Pageable pageable);
+
 
     Optional<Post> findByPostIdAndDeletedAtIsNull(Long postId);
 }
