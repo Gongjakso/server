e25369fa784814593c43dd163f008a11e8d54bdd
yumzen <yeom.ji.eun35@gmail.com>
2024-02-06 16:28:50 +0900

    feat: Post Controller 변경

--- src/main/java/com/gongjakso/server/domain/post/controller/PostController.java
+++ src/main/java/com/gongjakso/server/domain/post/controller/PostController.java
@@ -1,13 +1,18 @@
 package com.gongjakso.server.domain.post.controller;
 
+import com.gongjakso.server.domain.post.dto.GetProjectRes;
 import com.gongjakso.server.domain.post.dto.PostDeleteRes;
 import com.gongjakso.server.domain.post.dto.PostReq;
 import com.gongjakso.server.domain.post.dto.PostRes;
+import com.gongjakso.server.domain.post.repository.PostRepository;
 import com.gongjakso.server.domain.post.service.PostService;
 import com.gongjakso.server.global.common.ApplicationResponse;
 import com.gongjakso.server.global.security.PrincipalDetails;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 
     private final PostService postService;
+    private final PostRepository postRepository;
     @PostMapping("")
     public ApplicationResponse<PostRes> create(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody PostReq req) {
         return ApplicationResponse.ok(postService.create(principalDetails.getMember(), req));
@@ -32,11 +38,8 @@ public class PostController {
         return ApplicationResponse.ok(postService.delete(principalDetails.getMember(), id));
     }
 
-    /*
-
     @GetMapping("/project")
-    public ApplicationResponse<Page<GetProjectRes>> list(@PageableDefault(size = 6, sort = "created_at") Pageable pageable) {
-        return ApplicationResponse.ok(postService.getProjects(pageable));
+    public ApplicationResponse<Page<GetProjectRes>> list(@PageableDefault(size = 6) Pageable pageable) {
+        return ApplicationResponse.ok(postService.getProjects(pageable.getPageNumber(), pageable.getPageSize()));
     }
-    */
 }
