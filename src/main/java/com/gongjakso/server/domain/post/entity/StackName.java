a4d4d92d84ae06340026d4f33a5425d10cd66920
yumzen <yeom.ji.eun35@gmail.com>
2024-02-11 20:52:21 +0900

    [#42] feat: stack Name entity 생성

---
+++ src/main/java/com/gongjakso/server/domain/post/entity/StackName.java
@@ -0,0 +1,37 @@
+package com.gongjakso.server.domain.post.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gongjakso.server.domain.post.enumerate.StackNameType;
+import com.gongjakso.server.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "stack_name")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StackName extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stack_name_id", nullable = false, columnDefinition = "bigint")
+    private Long stackNameId;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false, columnDefinition = "bigint")
+    @JsonIgnore
+    private Post post;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "stack_name_type", nullable = false, columnDefinition = "varchar(20)")
+    private StackNameType stackNameType;
+
+    @Builder
+    public StackName(Post post, StackNameType stackNameType) {
+        this.post = post;
+        this.stackNameType = stackNameType;
+    }
+}
