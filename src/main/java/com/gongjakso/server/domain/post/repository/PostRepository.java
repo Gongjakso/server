6c0d372787a167d1d0334bef5a9c14feb6699fb7
yumzen <yeom.ji.eun35@gmail.com>
2024-02-13 15:16:09 +0900

    [#42] feat: 검색어 기반 조회 jpa 쿼리 메소드

--- src/main/java/com/gongjakso/server/domain/post/repository/PostRepository.java
+++ src/main/java/com/gongjakso/server/domain/post/repository/PostRepository.java
@@ -2,12 +2,15 @@ package com.gongjakso.server.domain.post.repository;
 
 import com.gongjakso.server.domain.post.dto.GetProjectRes;
 import com.gongjakso.server.domain.post.entity.Post;
+import com.gongjakso.server.domain.post.enumerate.PostStatus;
+import com.gongjakso.server.domain.post.enumerate.StackNameType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
@@ -17,12 +20,15 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<GetProjectRes> findAllProjects(Pageable pageable);
 
     Optional<Post> findByPostIdAndDeletedAtIsNull(Long postId);
-    @Query("SELECT new com.gongjakso.server.domain.post.dto.GetProjectRes(p.postId, p.title, m.name, p.status, p.startDate, p.finishDate) " +
-            "FROM Post p JOIN p.member m " +
-            "WHERE LOWER(FUNCTION('replace', p.title, ' ', '')) LIKE %:searchWord% " +
-            "and p.postType = true and p.deletedAt is null and p.finishDate >= CURRENT_TIMESTAMP and p.status = 'RECRUITING'")
-    Page<GetProjectRes> findBySearchWord(@Param("searchWord") String searchWord, Pageable pageable);
 
+    /*
+    검색어 기반 조회
+     */
+    Page<Post> findAllByTitleContainsAndPostTypeIsTrueAndDeletedAtIsNullAndFinishDateAfterAndStatus
+    (@Param("searchWord") String searchWord, @Param("currentTimestamp") LocalDateTime currentTimestamp, @Param("status") PostStatus status, Pageable pageable);
+
+    /*
+    스택 기반 조회
     @Query("SELECT new com.gongjakso.server.domain.post.dto.GetProjectRes(p.postId, p.title, m.name, p.status, p.startDate, p.finishDate) " +
             "FROM Post p JOIN p.member m JOIN p.stackNames sn " +
             "WHERE LOWER(FUNCTION('REPLACE', p.title, ' ', '')) LIKE %:searchWord% AND p.postType = true AND p.deletedAt IS NULL AND p.finishDate >= CURRENT_TIMESTAMP AND p.status = 'RECRUITING' " +
@@ -30,4 +36,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<GetProjectRes> findByStackNameAndSearchWord(@Param("stackName") String stackName,
                                                      @Param("searchWord") String searchWord,
                                                      Pageable pageable);
+     */
+    Page<Post> findAllByTitleContainsAndPostTypeIsTrueAndDeletedAtIsNullAndFinishDateAfterAndStatusAndStackNamesContaining
+            (@Param("searchWord") String searchWord, @Param("currentTimestamp") LocalDateTime currentTimestamp, @Param("status") PostStatus status, @Param("stackName") StackNameType stackName, @Param("pageable") Pageable pageable);
 }
