db43248c072095416b41561c9a781ebb9ac64a07
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2024-02-14 21:45:04 +0900

    [#41] test: Banner Unit test code 작성

---
+++ src/test/java/com/gongjakso/server/global/util/BannerUtilTest.java
@@ -0,0 +1,15 @@
+package com.gongjakso.server.global.util;
+
+import com.gongjakso.server.domain.banner.entity.Banner;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class BannerUtilTest {
+
+    public static List<Banner> createBannerList() {
+        return null;
+    }
+}
