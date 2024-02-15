738141c1b2f30677a2b34d241bbec7d84077bb41
yumzen <yeom.ji.eun35@gmail.com>
2024-02-15 18:56:39 +0900

    [#44] feat: 페이지 DTO 변경

--- src/main/java/com/gongjakso/server/domain/post/dto/GetContestRes.java
+++ src/main/java/com/gongjakso/server/domain/post/dto/GetContestRes.java
@@ -1,10 +1,12 @@
 package com.gongjakso.server.domain.post.dto;
 
+import com.gongjakso.server.domain.post.entity.Category;
 import com.gongjakso.server.domain.post.enumerate.PostStatus;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 public class GetContestRes {
@@ -15,10 +17,12 @@ public class GetContestRes {
     private LocalDateTime startDate;
     private LocalDateTime finishDate;
     private long daysRemaining;
+    private List<Category> categories;
+    private long scrapCount;
 
     @Builder
     public GetContestRes(Long postId, String title, String name, PostStatus status, LocalDateTime startDate,
-                         LocalDateTime finishDate){
+                         LocalDateTime finishDate, List<Category> categories,long scrapCount){
         this.postId = postId;
         this.title = title;
         this.name = name;
@@ -26,7 +30,7 @@ public class GetContestRes {
         this.startDate = startDate;
         this.finishDate = finishDate;
         this.daysRemaining = getDaysRemaining();
+        this.categories = categories;
+        this.scrapCount = scrapCount;
     }
-    // 파트명
-    // 스크랩 횟수
 }
