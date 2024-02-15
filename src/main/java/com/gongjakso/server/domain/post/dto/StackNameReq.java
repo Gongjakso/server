b78953c2f0fbf01e31519d2de2a2c54a02d7c44e
yumzen <yeom.ji.eun35@gmail.com>
2024-02-15 18:48:26 +0900

    [#51] feat: stackName-size 삭제

--- src/main/java/com/gongjakso/server/domain/post/dto/StackNameReq.java
+++ src/main/java/com/gongjakso/server/domain/post/dto/StackNameReq.java
@@ -1,13 +1,10 @@
 package com.gongjakso.server.domain.post.dto;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
-@Getter
 @NoArgsConstructor
 public class StackNameReq {
     private String stackNameType;
-    private Integer size;
 }
