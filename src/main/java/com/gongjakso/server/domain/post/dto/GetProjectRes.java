28e1e2bc528edc4ca166f20f8defb5562412091b
yumzen <yeom.ji.eun35@gmail.com>
2024-02-16 01:20:26 +0900

    [#63] fixed: daysRemaining, finishDate, endDate 수정

--- src/main/java/com/gongjakso/server/domain/post/dto/GetProjectRes.java
+++ src/main/java/com/gongjakso/server/domain/post/dto/GetProjectRes.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Getter
@@ -15,21 +16,21 @@ public class GetProjectRes {
     private String name; //팀장명
     private PostStatus status;
     private LocalDateTime startDate;
-    private LocalDateTime finishDate;
+    private LocalDateTime endDate;
     private long daysRemaining;
     private List<Category> categories;
     private long scrapCount;
 
     @Builder
     public GetProjectRes(Long postId, String title, String name, PostStatus status, LocalDateTime startDate,
-                         LocalDateTime finishDate, List<Category> categories,long scrapCount ){
+                         LocalDateTime endDate, LocalDateTime finishDate, List<Category> categories,long scrapCount ){
         this.postId = postId;
         this.title = title;
         this.name = name;
         this.status = status;
         this.startDate = startDate;
-        this.finishDate = finishDate;
-        this.daysRemaining = getDaysRemaining();
+        this.endDate = endDate;
+        this.daysRemaining = finishDate.isBefore(LocalDateTime.now()) ? -1 : ChronoUnit.DAYS.between(LocalDateTime.now(), finishDate);
         this.categories = categories;
         this.scrapCount = scrapCount;
     }
