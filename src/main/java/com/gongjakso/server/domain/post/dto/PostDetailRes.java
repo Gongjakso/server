b106473d5a0830b867fa7f8bc8cb48bfc6d301e2
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2024-02-15 21:32:55 +0900

    [#60] fix: 공고 상세 조회 API 반환 DTO 수정

---
+++ src/main/java/com/gongjakso/server/domain/post/dto/PostDetailRes.java
@@ -0,0 +1,67 @@
+package com.gongjakso.server.domain.post.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.gongjakso.server.domain.member.entity.Member;
+import com.gongjakso.server.domain.post.entity.Category;
+import com.gongjakso.server.domain.post.entity.Post;
+import com.gongjakso.server.domain.post.entity.StackName;
+import com.gongjakso.server.domain.post.enumerate.MeetingMethod;
+import com.gongjakso.server.domain.post.enumerate.PostStatus;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record PostDetailRes(
+        Long postId,
+        Long memberId,
+        String memberName,
+        String title,
+        String contents,
+        String urlLink,
+        PostStatus status,
+        LocalDateTime startDate,
+        LocalDateTime endDate,
+        Long daysRemaining,
+        Long maxPerson,
+        Long currentPerson,
+        List<StackName> stackNames,
+        List<Category> categories,
+        MeetingMethod meetingMethod,
+        String meetingArea,
+        boolean questionMethod,
+        String questionLink,
+        boolean postType,
+        LocalDateTime createdAt,
+        Long scrapCount
+) {
+
+    public static PostDetailRes of(Post post, Member member, Long currentPerson) {
+        return PostDetailRes.builder()
+                .postId(post.getPostId())
+                .memberId(member.getMemberId())
+                .memberName(member.getName())
+                .title(post.getTitle())
+                .contents(post.getContents())
+                .urlLink("https://www.naver.com")
+                .status(post.getStatus())
+                .startDate(post.getStartDate())
+                .endDate(post.getEndDate())
+                .daysRemaining(post.getFinishDate().isBefore(LocalDateTime.now()) ? -1 : ChronoUnit.DAYS.between(LocalDateTime.now(), post.getFinishDate()))
+                .maxPerson(post.getMaxPerson())
+                .currentPerson(currentPerson)
+                .stackNames(post.getStackNames())
+                .categories(post.getCategories())
+                .meetingMethod(post.getMeetingMethod())
+                .meetingArea(post.getMeetingArea())
+                .questionMethod(post.isQuestionMethod())
+                .questionLink(post.getQuestionLink())
+                .postType(post.isPostType())
+                .createdAt(post.getCreatedAt())
+                .scrapCount(post.getScrapCount())
+                .build();
+    }
+}
