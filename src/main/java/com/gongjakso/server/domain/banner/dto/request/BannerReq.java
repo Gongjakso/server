f8cfce2d5b91f2510c6cab4fb9f4042cb2f2a287
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2024-02-21 15:24:12 +0900

    [#84] feat: Banner 등록, 수정, 삭제, 게시 여부 변경 API

---
+++ src/main/java/com/gongjakso/server/domain/banner/dto/request/BannerReq.java
@@ -0,0 +1,40 @@
+package com.gongjakso.server.domain.banner.dto.request;
+
+import com.gongjakso.server.domain.banner.entity.Banner;
+import com.gongjakso.server.domain.banner.enumerate.DomainType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record BannerReq(
+    @Schema(
+            description = "배너 게시 위치를 (MAIN | PROJECT | CONTEST)의 ENUM으로 관리",
+            defaultValue = "MAIN"
+    )
+    @NotBlank DomainType domainType,
+    @Schema(
+        description = "배너 클릭 시, 리다이렉트될 주소",
+        defaultValue = "https://instagram.com/official_gongjakso"
+    )
+    String linkUrl,
+    @Schema(
+            description = "배너 게시 순서를 의미하며, 숫자가 낮을수록 높은 우선순위를 가짐",
+            defaultValue = "1"
+    )
+    @NotEmpty Integer priority,
+    @Schema(
+            description = "배너 삭제가 아닌 데이터로 가지고 있지만, 배너로 임시로 게시하지 않을 때 사용",
+            defaultValue = "true"
+    )
+    @NotNull Boolean isPost
+) {
+
+    public Banner from(String imageUrl) {
+        return Banner.builder()
+                .domainType(this.domainType)
+                .imageUrl(imageUrl)
+                .linkUrl(this.linkUrl)
+                .build();
+    }
+}
--- src/main/java/com/gongjakso/server/domain/banner/service/BannerService.java
+++ src/main/java/com/gongjakso/server/domain/banner/service/BannerService.java
@@ -1,12 +1,17 @@
 package com.gongjakso.server.domain.banner.service;
 
+import com.gongjakso.server.domain.banner.dto.request.BannerReq;
 import com.gongjakso.server.domain.banner.dto.response.BannerRes;
 import com.gongjakso.server.domain.banner.entity.Banner;
 import com.gongjakso.server.domain.banner.enumerate.DomainType;
 import com.gongjakso.server.domain.banner.repository.BannerRepository;
+import com.gongjakso.server.global.exception.ApplicationException;
+import com.gongjakso.server.global.exception.ErrorCode;
+import com.gongjakso.server.global.util.s3.S3Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +22,7 @@ import java.util.stream.Collectors;
 public class BannerService {
 
     private final BannerRepository bannerRepository;
+    private final S3Client s3Client;
 
     public List<BannerRes> getMainImageList() {
         // Business Logic
@@ -46,5 +52,65 @@ public class BannerService {
         return bannerList.stream()
                 .map(BannerRes::of)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public BannerRes registerBanner(BannerReq bannerReq, MultipartFile multipartFile) {
+        // Validation
+        if(multipartFile == null || multipartFile.isEmpty()) {
+            throw new ApplicationException(ErrorCode.INVALID_VALUE_EXCEPTION);
+        }
+
+        // Business Logic
+        String imageUrl = s3Client.upload(multipartFile, "banner");
+        Banner banner = bannerReq.from(imageUrl);
+        Banner savedBanner = bannerRepository.save(banner);
+
+        // Response
+        return BannerRes.of(savedBanner);
+    }
+
+    @Transactional
+    public BannerRes updateBanner(Long bannerId, BannerReq bannerReq, MultipartFile multipartFile) {
+        // Validation
+        Banner banner = bannerRepository.findById(bannerId).orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND_EXCEPTION));
+
+        // Business Logic
+        String imageUrl = "";
+        if(multipartFile != null && multipartFile.isEmpty()) {
+            s3Client.delete(banner.getImageUrl());
+            imageUrl = s3Client.upload(multipartFile, "banner");
+        }
+
+        banner.update(bannerReq, imageUrl);
+        Banner updatedBanner = bannerRepository.save(banner);
+
+        // Response
+        return BannerRes.of(updatedBanner);
+    }
+
+    @Transactional
+    public BannerRes changeIsPost(Long bannerId) {
+        // Validation
+        Banner banner = bannerRepository.findById(bannerId).orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND_EXCEPTION));
+
+        // Business Logic
+        banner.changeIsPost();
+        Banner updatedBanner = bannerRepository.save(banner);
+
+        // Response
+        return BannerRes.of(updatedBanner);
+    }
+
+    @Transactional
+    public void deleteBanner(Long bannerId) {
+        // Validation
+        Banner banner = bannerRepository.findById(bannerId).orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND_EXCEPTION));
+
+        // Business Logic
+        s3Client.delete(banner.getImageUrl());
+        bannerRepository.delete(banner);
+
+        // Response
     }
 }
