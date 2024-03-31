cdaa40f1203177558ba73a919406255b088842fa
jinnxyoung <wlsdud7722@naver.com>
2024-03-31 18:22:15 +0900

    [#71] test: post 클래스 test 코드 작성중...

---
+++ src/test/java/com/gongjakso/server/domain/post/service/PostServiceTest.java
@@ -0,0 +1,39 @@
+package com.gongjakso.server.domain.post.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.gongjakso.server.domain.member.entity.Member;
+import com.gongjakso.server.domain.post.entity.Post;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class PostServiceTest {
+
+    @Autowired
+    PostService postService;
+    Post post = null;
+
+    @BeforeEach
+    void beforeEach() {
+
+    }
+
+    @Test
+    void create() {
+        
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void modify() {
+    }
+
+    @Test
+    void delete() {
+
+    }
+}
