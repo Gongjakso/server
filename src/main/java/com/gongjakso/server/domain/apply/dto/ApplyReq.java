b55a75d1a39bc1bf52d2723ef02c044f0d32787c
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-04-03 17:49:53 +0900

    #95 StackName Contest경우 null 허용

--- src/main/java/com/gongjakso/server/domain/apply/dto/ApplyReq.java
+++ src/main/java/com/gongjakso/server/domain/apply/dto/ApplyReq.java
@@ -10,40 +10,53 @@ import com.gongjakso.server.domain.post.entity.Post;
 import com.gongjakso.server.domain.post.entity.StackName;
 import com.gongjakso.server.global.exception.ApplicationException;
 import com.gongjakso.server.global.exception.ErrorCode;
+import jakarta.validation.constraints.Null;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApplyReq(
         String application,
         String recruit_part,
         String recruit_role,
         String type,
         String applyType,
+        @Null
         List<String> stackNames
 ) {
     public Apply toEntity(Member member, Post post){
-
-        List<StackType> stackTypeList = new ArrayList<>();
-        for (String stackName : stackNames) {
-            try {
-                StackType stackType = StackType.valueOf(stackName.toUpperCase());
-                stackTypeList.add(stackType);
-            } catch (IllegalArgumentException e) {
-                throw new ApplicationException(ErrorCode.INVALID_STACK_TYPE_EXCEPTION);
+        if(type.equals("CONTEST")){
+            return  Apply.builder()
+                    .member(member)
+                    .post(post)
+                    .application(application)
+                    .recruit_part(recruit_part)
+                    .recruit_role(recruit_role)
+                    .type(PostType.valueOf(type))
+                    .applyType(ApplyType.NONE)
+                    .build();
+        }else {
+            List<StackType> stackTypeList = new ArrayList<>();
+            for (String stackName : stackNames) {
+                try {
+                    StackType stackType = StackType.valueOf(stackName.toUpperCase());
+                    stackTypeList.add(stackType);
+                } catch (IllegalArgumentException e) {
+                    throw new ApplicationException(ErrorCode.INVALID_STACK_TYPE_EXCEPTION);
+                }
             }
-        }
 
-        return Apply.builder()
-                .member(member)
-                .post(post)
-                .application(application)
-                .recruit_part(recruit_part)
-                .recruit_role(recruit_role)
-                .type(PostType.valueOf(type))
-                .applyType(ApplyType.NONE)
-                .stackTypeList(stackTypeList)
-                .build();
+            return Apply.builder()
+                    .member(member)
+                    .post(post)
+                    .application(application)
+                    .recruit_part(recruit_part)
+                    .recruit_role(recruit_role)
+                    .type(PostType.valueOf(type))
+                    .applyType(ApplyType.NONE)
+                    .stackTypeList(stackTypeList)
+                    .build();
+        }
     }
 }
