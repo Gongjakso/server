7d4878afb6a133c6d301c47234d86d550972dab7
yumzen <yeom.ji.eun35@gmail.com>
2024-04-06 15:19:33 +0900

    fixed: operation 수정

--- src/main/java/com/gongjakso/server/domain/post/controller/PostController.java
+++ src/main/java/com/gongjakso/server/domain/post/controller/PostController.java
@@ -130,7 +130,7 @@ public class PostController {
         return ApplicationResponse.ok(postService.scrapGet(principalDetails.getMember(), id));
     }
 
-    @Operation(summary = "내가 스크랩한 공고 조회 API", description = "프로젝트/공모전 별 각 한 개씩 묶어서 리스트 형태로 반환")
+    @Operation(summary = "내가 모집 중인 팀 조회 API", description = "프로젝트/공모전 별 각 한 개씩 묶어서 리스트 형태로 반환")
     @GetMapping("/my")
     public ApplicationResponse<List<MyPageRes>> getMyPostList(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         return ApplicationResponse.ok(postService.getMyPostList(principalDetails.getMember()));
