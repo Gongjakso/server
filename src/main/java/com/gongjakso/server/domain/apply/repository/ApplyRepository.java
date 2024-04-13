f6216d6d8fc3504b23fd4b10295a9e1df771e7b4
Jieun Yeom <yeom.ji.eun35@gmail.com>
2024-04-13 19:30:03 +0900

    fix: ApplyRepository 충돌

--- src/main/java/com/gongjakso/server/domain/apply/repository/ApplyRepository.java
+++ src/main/java/com/gongjakso/server/domain/apply/repository/ApplyRepository.java
@@ -12,8 +12,11 @@ import java.util.List;
 
 public interface ApplyRepository extends JpaRepository<Apply,Long> {
     long countApplyWithStackNameUsingFetchJoinByPost(Post post);
+    long countApplyByPost(Post post);
     boolean existsApplyByMemberAndPost(Member member,Post post);
     Page<Apply> findAllByPost(Post post, Pageable pageable);
     Page<Apply> findApplyByApplyType(ApplyType applyType, Pageable pageable);
     List<Apply> findAllByMemberAndDeletedAtIsNull(Member member);
+    List<Apply> findAllByPost(Post post);
+    Long findApplyIdByMemberAndPost(Member member,Post post);
 }
