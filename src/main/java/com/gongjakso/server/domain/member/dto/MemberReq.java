52dfd954ee6db363d3a550a822466fa51d7b8731
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2024-04-18 00:37:59 +0900

    [#106] fix: 유효성 검증을 위한 정규표현식 추가

--- src/main/java/com/gongjakso/server/domain/member/dto/MemberReq.java
+++ src/main/java/com/gongjakso/server/domain/member/dto/MemberReq.java
@@ -1,11 +1,12 @@
 package com.gongjakso.server.domain.member.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
 @Builder
 public record MemberReq(@NotNull String name,
-                        String phone,
+                        @Pattern(regexp = "^010-[0-9]{4}-[0-9]{4}$") String phone,
                         String status,
                         String major,
                         String job) {
