ed1e798ea6b175d100152dd5f764f90d221df1b4
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2024-05-18 16:17:21 +0900

    [#128] fix: 정렬 기준을 생성 시각 기준 내림차순으로 적용 관련 오류 수정

--- src/main/java/com/gongjakso/server/domain/apply/service/ApplyService.java
+++ src/main/java/com/gongjakso/server/domain/apply/service/ApplyService.java
@@ -168,7 +168,7 @@ public class ApplyService {
             throw new ApplicationException(ErrorCode.UNAUTHORIZED_EXCEPTION);
         }
 
-        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "created_at"));
+        Pageable pageable = PageRequest.of(page, size, Sort.by("created_at").ascending());
         Page<Apply> applyPage = applyRepository.findAllByPost(post, pageable);
         List<ApplyList> applyLists = applyPage.getContent().stream()
                 .map(apply -> ApplyList.of(apply, decisionState(apply)))
