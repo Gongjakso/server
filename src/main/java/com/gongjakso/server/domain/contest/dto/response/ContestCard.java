01695e998b1484144d87d5319765ab7a227513bc
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-08-14 22:40:47 +0900

    #172 feat: add dayState
    ContestCard에 dayState 추가

--- src/main/java/com/gongjakso/server/domain/contest/dto/response/ContestCard.java
+++ src/main/java/com/gongjakso/server/domain/contest/dto/response/ContestCard.java
@@ -3,11 +3,14 @@ package com.gongjakso.server.domain.contest.dto.response;
 import com.gongjakso.server.domain.contest.entity.Contest;
 import lombok.Builder;
 
+import static com.gongjakso.server.domain.contest.dto.response.ContestRes.text;
+
 @Builder
 public record ContestCard(
         long id,
         String title,
         String imgUrl,
+        String dayState,
         int teamCount
 ) {
     public static ContestCard of(Contest contest,int teamCount){
@@ -15,6 +18,7 @@ public record ContestCard(
                 .id(contest.getId())
                 .title(contest.getTitle())
                 .imgUrl(contest.getImgUrl())
+                .dayState(text(contest.getStartedAt(),contest.getFinishedAt()))
                 .teamCount(teamCount)
                 .build();
     }
