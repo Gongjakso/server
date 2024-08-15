2194fce5ace523ee7927afb991c6c7974d36e838
dl-00-e8 <sso07012@gmail.com>
2024-08-15 13:11:21 +0900

    [#173] refactor: 서버 시작 로그 출력 방식 변경

--- src/main/java/com/gongjakso/server/ServerApplication.java
+++ src/main/java/com/gongjakso/server/ServerApplication.java
@@ -1,24 +1,19 @@
 package com.gongjakso.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
+@Slf4j
 @EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-		System.out.println("Gongjakso Server is running");
-	}
-
-	@Bean
-	public PageableHandlerMethodArgumentResolverCustomizer customize() {
-		return p -> {
-			p.setOneIndexedParameters(true);	// 1부터 시작
-		};
+		log.info("Gongjakso Server is running");
 	}
 }
