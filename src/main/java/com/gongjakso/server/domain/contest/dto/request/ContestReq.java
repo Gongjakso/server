3c9334046f1af3133dc9e3c3edb5a6995e6c4dce
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-08-16 15:33:30 +0900

    #177 feat : valid annotation
    ContestReq에 valid annotation 추가

--- src/main/java/com/gongjakso/server/domain/contest/dto/request/ContestReq.java
+++ src/main/java/com/gongjakso/server/domain/contest/dto/request/ContestReq.java
@@ -2,6 +2,7 @@ package com.gongjakso.server.domain.contest.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -11,14 +12,20 @@ import java.time.LocalDate;
 
 @Builder
 public record ContestReq(
+        @NotBlank
         @Size(min=1,max=150)
         String title,
+        @NotEmpty
         String body,
+        @NotEmpty
         String contestLink,
+        @NotNull
         @Size(min=1,max=100)
         String institution,
+        @NotNull
         @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate startedAt,
+        @NotNull
         @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate finishedAt
 
