7b8efd66f5e247efb161198497a0cfb3a77c2ec4
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-08-19 01:17:03 +0900

    #186 feat : redis cache config

--- src/main/java/com/gongjakso/server/global/config/CacheConfig.java
+++ src/main/java/com/gongjakso/server/global/config/CacheConfig.java
@@ -5,10 +5,23 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @EnableCaching
 @Configuration
 public class CacheConfig {
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    public CacheConfig(RedisConnectionFactory redisConnectionFactory) {
+        this.redisConnectionFactory = redisConnectionFactory;
+    }
 
     @Bean
     public CacheManager cacheManager() {
@@ -18,5 +31,18 @@ public class CacheConfig {
         cacheManager.setAllowNullValues(false);
 
         return cacheManager;
+    }
+
+    @Bean
+    public CacheManager redisCacheManager() {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60)) // 캐시 TTL 설정
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .transactionAware()
+                .build();
     }
 }
