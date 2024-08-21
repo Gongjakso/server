3e54fee40503cf26b4f87af4d85d142d9cc025ee
sycuuui <102959791+sycuuui@users.noreply.github.com>
2024-08-22 00:26:19 +0900

    #186 feat : 수정 어노테이션 추가

--- src/main/java/com/gongjakso/server/domain/contest/repository/ContestRepository.java
+++ src/main/java/com/gongjakso/server/domain/contest/repository/ContestRepository.java
@@ -1,13 +1,14 @@
 package com.gongjakso.server.domain.contest.repository;
 
 import com.gongjakso.server.domain.contest.entity.Contest;
-import io.lettuce.core.dynamic.annotation.Param;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ContestRepository extends JpaRepository<Contest, Long>, ContestRepositoryCustom {
-    @Query("update Contest c set c.view = c.view + 1 where c.id = :contestId")
-    void updateView(@Param("contestId") long contestId);
+    @Modifying
+    @Query("UPDATE Contest c SET c.view = c.view + 1 WHERE c.id = :id")
+    void updateView(@Param("id") Long id);
 }
