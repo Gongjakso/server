de1e4dd24d2904a2e13235f1c54772770e2ebc6b
jinnxyoung <wlsdud7722@naver.com>
2024-09-06 00:24:57 +0900

    [#197] feat: Portfolio Entity 수정

--- src/main/java/com/gongjakso/server/domain/portfolio/entity/Portfolio.java
+++ src/main/java/com/gongjakso/server/domain/portfolio/entity/Portfolio.java
@@ -1,21 +1,35 @@
 package com.gongjakso.server.domain.portfolio.entity;
 
+import com.gongjakso.server.domain.portfolio.vo.PortfolioData;
 import com.gongjakso.server.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
+import java.time.LocalDate;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.type.SqlTypes;
 
 @Getter
 @Entity
 @Table(name = "portfolio")
-@SQLDelete(sql = "UPDATE portfolio SET deleted_at = NOW() where id = ?")
+@SQLDelete(sql = "UPDATE portfolio SET deleted_at = NOW() where portfolio_id = ?")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Portfolio extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id",nullable = false,columnDefinition = "bigint")
+    @Column(name = "portfolio_id", nullable = false, columnDefinition = "bigint")
     private Long id;
+
+    @Column(name = "portfolio_data", columnDefinition = "json")
+    @JdbcTypeCode(SqlTypes.JSON)
+    private PortfolioData portfolioData;
+
+    @Builder
+    public Portfolio(PortfolioData portfolioData) {
+        this.portfolioData = portfolioData;
+    }
 }
